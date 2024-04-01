70bb619ec1efc997f65d9be99f11715e0adeca77
Himany <Himaaan@yandex.ru>
2024-04-02 02:11:23 +0300

    rework pm2 deploy

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,7 +1,8 @@
-require('dotenv').config({ path: "./.env.deploy" });
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env.deploy" });
 
 const {
-  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master', DEPLOY_REPO,
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPOSITORY,
 } = process.env;
 
 module.exports = {
@@ -16,10 +17,10 @@ module.exports = {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: DEPLOY_REPO,
+      repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': `cd ${DEPLOY_PATH} npm i && npm run build && pm2 start ecosystem.config.js --env production`,
+      'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
+      'post-deploy': `cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production`,
     },
   },
 };
