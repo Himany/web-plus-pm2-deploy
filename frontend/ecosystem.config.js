cf2d910ad893c63aad925138421fde6f635064c1
Himany <Himaaan@yandex.ru>
2024-04-02 00:40:18 +0300

    add crush-test

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,24 @@
+require('dotenv').config({ path: "./.env.deploy" });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master', DEPLOY_REPO,
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'mesto-frontend'
+  }],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'npm i && npm run build',
+    },
+  },
+};
